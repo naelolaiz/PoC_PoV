aa248c09bf1a015770b86ab08a5579818f3323dc
nael <naelblog@gmail.com>
2024-04-10 19:22:04 +0200

    added default secrets.h

---
+++ src/secrets.h
@@ -0,0 +1,7 @@
+#pragma once
+
+const char* ssid_ap = "access_point_esp32_name";
+const char* password_ap = "MyPassword";
+
+const char* ssid_sta = "access_point_router_name";
+const char* password_sta = "MyPassword";
